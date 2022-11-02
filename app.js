93144927c6df211fe1d681c17f0a7ca801f6361c
Mahmoud-Almohammad <almohammadmhmood@gmail.com>
2022-11-02 21:17:24 +0300

    final version

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log('hello');
+
+confirm('Do you want to receive notification from our site?')
+
+prompt('what is the time now?')
+
+var hour = 10;
+
+if (hour >= 6 && hour <12)
+console.log('Good morning');
+else if (hour >=12 && hour <18)
+console.log('Good afternoon');
+else
+console.log('Good evining');
