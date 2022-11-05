0981cc77b1073e6c2920f9bfd6f8d4e4c9e05cf2
Mahmoud-Almohammad <almohammadmhmood@gmail.com>
2022-11-06 01:59:21 +0300

    new version

--- app.js
+++ app.js
@@ -2,13 +2,31 @@ console.log('hello');
 
 confirm('Do you want to receive notification from our site?')
 
-prompt('what is the time now?')
+var Usertime = prompt('what is the time now?')
 
-var hour = 10;
-
-if (hour >= 6 && hour <12)
+if (Usertime >= 6 && Usertime <12)
 console.log('Good morning');
-else if (hour >=12 && hour <18)
+else if (Usertime >=12 && Usertime <18)
 console.log('Good afternoon');
 else
 console.log('Good evining');
+
+var breakingbad = prompt('Say my name');
+
+while (breakingbad === '' || breakingbad === null || breakingbad !== 'heisenberg') {
+    breakingbad = prompt('Say my name');
+}
+
+var imageShown = prompt('how many books do you want to read from 1-5?')
+
+for (var i = 1; i <=  imageShown; i++) {
+    document.write('<img src="books.jpg">');
+}
+
+for (i = 1;(Number(imageShown) > 5 || Number(imageShown) < 1); i++) {
+    imageShown = prompt('how many books do you want to read from 1-5?')
+}
+
+
+
+
